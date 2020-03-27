4d309db56578dfec063aea689535e61ac16dd419
dsenatec <dsenatec@gmail.com>
2020-03-27 10:45:48 -0300

    Back-end da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const OngController = require('./controller/OngController');
+const IncidentController = require('./controller/IncidentController');
+const ProfileController = require('./controller/ProfileController');
+const SessionController = require('./controller/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', OngController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents', IncidentController.delete);
+
+//Exportar variáveis de um arquivo
+module.exports = routes;
